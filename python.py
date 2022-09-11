a1a913268241958c0e5382fcd65676e691b1aacc
kgoodman <kellangoodman@gmail.com>
2022-09-11 14:34:42 -0700

    user with bank account

--- python.py
+++ python.py
@@ -1,4 +1,28 @@
+class user:
+
+    def __init__(self, name="0", starting_balance=0, interest_rate=0.03):
+        self.name = name
+        self.account = BankAccount(starting_balance, interest_rate)
+
+    def display_user_balance(self):
+        print(self.name)
+        self.account.display_account_info()
+        return self
+
+    def make_deposit(self, amount):
+        self.account.deposit(amount)
+        return self
+
+    def yield_interest(self):
+        self.account.yield_interest()
+        return self
+
+    def make_withdraw(self, amount):
+        self.account.withdraw(amount)
+        return self
+
 class BankAccount:
+    bankname = "the dojo trust"
     accounts = []
     def __init__(self, int_rate, balance): 
         self.int_rate = int_rate
@@ -28,6 +52,13 @@ class BankAccount:
         return self
 
     @classmethod
+    def all_balances(acnt):
+        sum = 0
+        for account in acnt.all_accounts:
+            sum += account.balance
+        return sum
+
+    @classmethod
     def print_all(acnt):
         for account in acnt.accounts:
             account.display_account_info()
@@ -41,3 +72,8 @@ checking.deposit(300).deposit(100).withdraw(20).withdraw(50).withdraw(15).withdr
 BankAccount.print_all()
 
 
+
+ryan = user("ryan glookle")
+ryan.display_user_balance().make_deposit(55).display_user_balance()
+ryan.make_withdraw(22).display_user_balance()
+ryan.yield_interest().display_user_balance()
